bbcb7a5d1ce9876d20b855185a466f3e20fc82e8
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-02-19 00:04:07 +0530

    Profile Completion

--- app/Controller/userController.js
+++ app/Controller/userController.js
@@ -7,7 +7,11 @@ userController.getProfile = async (req, res) => {
     const user = await User.findOne({
       where: { id: req.id },
       attributes: ["name", "username", "mobile"],
-      include: { model: Address, attributes: { exclude: ["UserId"] } },
+      include: {
+        model: Address,
+        attributes: { exclude: ["UserId"] },
+      },
+      order: [[Address, "id", "DESC"]],
     });
     if (user) {
       res.send({ status: 1, message: "Profile found.", data: user.toJSON() });
@@ -23,6 +27,48 @@ userController.getProfile = async (req, res) => {
       status: 0,
       message: "Some issue while getting profile for the user.",
     });
+  }
+};
+
+userController.setAddress = async (req, res) => {
+  try {
+    const user = await Address.create({ ...req.body, UserId: req.id });
+    res.send({ status: 1, message: "Address Created Successfully!!", user });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 0,
+      message: "Caught into some issue while adding address.",
+    });
+  }
+};
+
+userController.deleteAddress = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await Address.destroy({ where: { id } });
+    res.send({ status: 1, message: "Address deleted successfully.", data });
+  } catch (error) {
+    res.send({ status: 0, message: "Some issue while deleting the address" });
+  }
+};
+
+userController.updateAddress = async (req, res) => {
+  const { id } = req.body;
+  delete req.body.id;
+  try {
+    const [data] = await Address.update({ ...req.body }, { where: { id } });
+    if (data === 1) {
+      res.send({ status: 1, message: "Address updated Successfully!" });
+    } else {
+      res.send({
+        status: 0,
+        message: "Some issue while updating Address!",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: 0, message: "Some issue while updating Address!" });
   }
 };
 export default userController;
--- app/Router/userRoutes.js
+++ app/Router/userRoutes.js
@@ -2,8 +2,12 @@ import express from "express";
 import { isAuthenticated } from "../MiddleWares/Authentication.js";
 import userController from "../Controller/userController.js";
 const router = express.Router();
+
 router.use(isAuthenticated);
 
 router.get("/profile", userController.getProfile);
+router.post("/address", userController.setAddress);
+router.delete("/address/:id", userController.deleteAddress);
+router.put("/address", userController.updateAddress);
 
 export default router;
