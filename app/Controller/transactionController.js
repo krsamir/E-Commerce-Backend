08de677e0dde99b388c5d37cd0b63fdd139233d1
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-05-06 10:38:26 +0530

    cart issue

--- app/Controller/transactionController.js
+++ app/Controller/transactionController.js
@@ -19,14 +19,14 @@ transactionController.getCart = async (req, res) => {
   const [item] = JSON.parse(JSON.stringify(rows));
   const { Products: product, name } = item;
   if (product.length) {
-    const data = product
-      .slice(0, 4)
-      .map(({ name, offerprice, actualprice, productCode }) => ({
+    const data = product.map(
+      ({ name, offerprice, actualprice, productCode }) => ({
         name,
         offerprice,
         actualprice,
         productCode,
-      }));
+      })
+    );
     res.send({ count: product.length, name, data });
   } else {
     res.send({ count: 0, name, data: [] });
